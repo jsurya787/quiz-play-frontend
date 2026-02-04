79669e73e1fcb9ee8813b1f39451eb8f91f4b4d1
jaisurya <jsurya787@gmail.com>
2026-02-04 14:36:35 +0530

    ..

--- environment.ts
+++ environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   //googleClientId:'258326031404-hdqc40qajfvrlvi6v85ua8la1ncds6fe.apps.googleusercontent.com',
   googleClientId:'258326031404-pc1k8if7kunh5k15btme3p2eni5a4ula.apps.googleusercontent.com',
-  //apiUrl : "https://api.quizplay.co.in",
-  apiUrl :'http://localhost:3000',
+  apiUrl : "https://api.quizplay.co.in",
+  //apiUrl :'http://localhost:3000',
   //http://56.228.33.41:3000/
 };
